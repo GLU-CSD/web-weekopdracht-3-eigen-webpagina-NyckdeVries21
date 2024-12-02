cd31c88926912e517ef02000b45b74ddf7ec6c98
NyckdeVries21 <michaelwitkamp16@gmail.com>
2024-12-02 09:29:41 +0100

    Week3 Web dev.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("Hello, World!");
+// alert("Hello, World!");
+
+
+let schumacherfoto;
+
+function preload() {
+    schumacherfoto = loadImage('/assets/michaelschumacher.jpg'); 
+}
+
+function setup() {
+    createCanvas(400, 400); 
+    let button = createButton("Beste coureurs van allertijden");
+    button.position(100, 150);
+
+    
+    image(schumacherfoto, 100, 200, 200, 150);
+}
+
+
+
+
